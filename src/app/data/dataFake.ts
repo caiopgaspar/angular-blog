de0e85cea2762ba094f4ddada5793de68331b2c6
caiopgaspar <caio_gaspar@yahoo.com.br>
2024-02-26 17:53:40 -0300

    content creation

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"The Last Kingdom",
+    "description":"Série da BBC e Netflix terá nova temporada em 2025, que substituirá o filme lançado em 2023 'Seven Kings Must Die'.",
+    "photoCover":"https://www.denofgeek.com/wp-content/uploads/2022/03/The-Last-Kingdom-Season-5-poster-cropped.jpg?fit=1200%2C675"
+  },
+  {
+    "id":"2",
+    "title":"The Sandman",
+    "description":"descrição2",
+    "photoCover":"https://p2.trrsf.com/image/fget/cf/1200/675/middle/images.terra.com/2022/11/03/428-netflix-anuncia-segunda-temporada-de-sandman-skv1yujva246.jpg"
+  },
+]
